77b1e447de6b1b4c54a6a2966c813451b3862e94
Ramon Carvalho Maciel <Ramon@ramonmacbook.local>
2014-11-30 02:22:53 -0200

    Removendo echo de teste

--- bin/prepareFolders.sh
+++ bin/prepareFolders.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "pinto"
 find . -maxdepth 4 -type d -name 'subs' | while read path; do mv -v $path $(echo $path | sed 's/instaladas\///; s/\//-/g; s/-/\//'); done
 find . -maxdepth 1 -mindepth 1 -type d -not -name '*-subs' -print0 | xargs -0 rm -rf
